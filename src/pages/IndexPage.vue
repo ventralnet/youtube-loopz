38e04430b71435b547a85c71d13f9c52fca4f142
Matt Kirkley <matt.kirkley@gmail.com>
2024-10-27 14:10:05 -0400

    tmp

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
-    Hi
+  <q-page class="bg-primary">
+    <div class="column" style="">
+      <div class="col bg-positive">
+        <q-video style="height: 25rem" src="https://www.youtube.com/embed/k3_tw44QsZQ?rel=0" />
+      </div>
+      <div class="col bg-warning">
+        controls
+      </div>
+    </div>
   </q-page>
 </template>
 

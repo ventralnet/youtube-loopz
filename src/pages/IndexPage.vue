06abc4faf61ea268c731d2f68e753c4be33f11cb
Matt Kirkley <matt.kirkley@gmail.com>
2024-11-11 06:15:20 -0500

    asdfasd

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,20 @@
 <template>
-  <q-page class="bg-primary">
-    <div class="q-video">
-      <div
-ref="youtube"></div>
+  <q-page class="">
+    <div class="text-center full-width q-pt-lg">
+      <div class="q-video inline-block">
+        <div ref="youtube"></div>
+      </div>
+      <div class="q-mx-lg">
+        <div style="width: 15rem">
+          {{ playbackRate }}
+          <q-slider
+            v-model="playbackRate"
+            :min="0"
+            :max="1.0"
+            :step="0.1"
+            @update:model-value="updatePlaybackRate" />
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
@@ -17,6 +29,13 @@ defineOptions({
 
 const videoId = ref('dQw4w9WgXcQ');
 const youtube = ref();
+const playbackRate = ref(3.5);
+let videoEle = null;
+
+const updatePlaybackRate = (newValue) => {
+  videoEle.setPlaybackRate(newValue);
+};
+
 const { onReady } = usePlayer(videoId, youtube, {
   cookie: false,
   playerVars: {
@@ -24,9 +43,18 @@ const { onReady } = usePlayer(videoId, youtube, {
   },
 });
 onReady((event) => {
-  event.target.playVideo();
+  videoEle = event.target;
+  // event.target.playVideo();
+  event.target.setPlaybackRate(5.5);
 });
 onMounted(() => {
   console.log(youtube);
 });
 </script>
+
+<style scoped>
+.q-video {
+  height: 20rem;
+  width: 40rem;
+}
+</style>
